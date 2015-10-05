38cabee9e29022fc5c055f85cdcd989ea265eec2
Ken <kenlsm@u.nus.edu>
2015-10-05 16:12:57 +0800

    added delete and update

--- src/common/CelebiBag.java
+++ src/common/CelebiBag.java
@@ -29,9 +29,19 @@ public class CelebiBag implements Iterable<Celebi> {
 		return c;
 	}
 	
+	
+	public Celebi removeCelebi(int index){
+		assert index >= 0 : index;
+		assert index < celebis.size() - 1: index;
+		
+		Celebi rtnCelebi = celebis.remove(index);
+		return rtnCelebi;
+	}
+	
 	public int size(){
 		return celebis.size();
 	}
+	
 	/*
 	 * Sort will return a new container with sorted type
 	 */
@@ -39,7 +49,7 @@ public class CelebiBag implements Iterable<Celebi> {
 		CelebiBag newContainer = new CelebiBag();
 		return newContainer;
 	}
-
+	
 	@Override
 	public Iterator<Celebi> iterator() {
 		// TODO Auto-generated method stub
--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -1,5 +1,7 @@
 package logic;
 
+import java.util.Date;
+
 import common.Celebi;
 import common.CelebiBag;
 import parser.Command;
@@ -34,20 +36,21 @@ public class Logic implements LogicInterface {
 	public Command executeCommand(String cmd) throws IntegrityCommandException {
 		Command rtnCmd = parser.parseCommand(cmd);
 		Celebi rtnCelebi = null;
-		
-		try
-		{
-			verifyCommand(rtnCmd);
-		}
-		catch(IntegrityCommandException e){
-			throw e;
-		}
-		
-		
+				
 		switch (rtnCmd.getCmdType()) {
 			case Add:
 				rtnCelebi = createCelebi(rtnCmd);
 				storage.save(rtnCelebi);
+				break;
+				
+			case Delete:
+
+				doDelete(rtnCmd);
+				break;
+			
+			case Update:
+				
+				doUpdate(rtnCmd);
 				break;
 				
 			case Quit:
@@ -57,20 +60,57 @@ public class Logic implements LogicInterface {
 			case Invalid:
 				System.out.println("Logic received invalid type");
 				throw new IntegrityCommandException("invalid thrown from parser");
+				
 			default:
+				assert false : rtnCmd.getCmdType();
 				break;
 		}
 		return rtnCmd;
 	}
 
-	private Celebi createCelebi(Command rtnCmd) {
-		// TODO Auto-generated method stub
-		Celebi tCelebi = new Celebi("Logic dummy celebi", null, null);
+	private void doUpdate(Command rtnCmd) {
+		int index = Integer.parseInt(rtnCmd.getName());
+		Celebi recvCelebi = mBag.getCelebi(index);
 		
-		if(true);	// hasDate data
+		recvCelebi.setName(rtnCmd.getDescr());
+	}
 
+	private void doDelete(Command rtnCmd) {
+		int index = Integer.parseInt(rtnCmd.getName());
+		Celebi recvCelebi = mBag.getCelebi(index);
+		boolean delStatus = storage.delete(recvCelebi);
+		
+		if(delStatus){	// Removed successfully
+			mBag.removeCelebi(index);
+		}else{
+			// Throw error?
+		}
+		
+	}
+
+	private Celebi createCelebi(Command rtnCmd) throws IntegrityCommandException {
+		// TODO Auto-generated method stub
+		try
+		{
+			verifyCommand(rtnCmd);
+		}
+		catch(IntegrityCommandException e){
+			throw e;
+		}
+		
+		String name = rtnCmd.getName();
+		Date startDate = rtnCmd.getStart();
+		Date endDate = rtnCmd.getEnd();
+		Celebi tCelebi = new Celebi(name, startDate, endDate);
+		
+		boolean addStatus = storage.save(tCelebi);
+		if(addStatus){	// Added successfully
+			mBag.addCelebi(tCelebi);
+		}else{
+			// Throw?
+		}
 			
-		mBag.addCelebi(tCelebi);
+		
 		return tCelebi;
 	}
 
