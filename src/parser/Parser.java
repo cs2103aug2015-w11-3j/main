12ff65183023085e27bc90af6d95b23fcbcc567c
Yijin <leowyijin@gmail.com>
2015-10-06 11:38:09 +0800

    added methods to directly generate command objects for testing

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,30 +1,82 @@
 package parser;
 
 import com.sun.javafx.css.Combinator;
+import common.Celebi;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
+import java.util.Date;
 import java.util.regex.*;
 
 public class Parser implements ParserInterface {
 
-	// private static final 
+	// Patterns for user command matching for substring after 
+	private static final Pattern P_ADD = Pattern.compile(
+			"(?<>)");
+	private static final Pattern P_DEL = Pattern.compile(
+			"()");
+	private static final Pattern P_UPD = Pattern.compile(
+			"()");
+	
+	public Command makeAdd (String name, Date start, Date end) {
+		Command cmd = new Command(Command.Type.Add, "test methods no raw user input");
+		cmd.setEnd(end);
+		cmd.setStart(start);
+		cmd.setName(name);
+		return cmd;
+	}
+	
+	public Command makeUpdate (int taskUID, Celebi.DataType fieldType, Object newValue) {
+		Command cmd = new Command(Command.Type.Update, "test methods no raw user input");
+		cmd.setTaskField(fieldType);
+		cmd.setTaskUID(taskUID);
+		switch (fieldType) {
+		case NAME : 
+			cmd.setName((String) newValue);
+			break;
+		case DATE_START :
+			cmd.setStart((Date) newValue);
+			break;
+		case DATE_END :
+			cmd.setEnd((Date) newValue);
+			break;
+		default :
+			throw new IllegalArgumentException("Allowed fields are name, start, and end dates.");
+			// break;
+		}
+		return cmd;	
+	}
+
+	public Command makeDelete (int taskUID) {
+		Command cmd = new Command(Command.Type.Delete, "test methods no raw user input");
+		cmd.setTaskUID(taskUID);
+		return cmd;
+	}
+	
+	public Command makeQuit () {
+		Command cmd = new Command(Command.Type.Quit, "test methods no raw user input");
+		return cmd;
+	}
+	
+	public Command makeInvalid () {
+		Command cmd = new Command(Command.Type.Invalid, "test methods no raw user input");
+		return cmd;
+	}
 	
 	@Override
 	public void init () {
-		// TODO Auto-generated method stub
 		System.out.println("Parser Init");
 		System.out.println("Parser Init complete");
 	}
 
 	@Override
 	public Command parseCommand (String rawInput) {
-		Command.Type type = parseCmdType(rawInput);
+		//String[] cmdAndDet
+		Command.Type type = getCmdType(rawInput);
 		return null;
 	}
 	
-	private Command.Type parseCmdType (String rawInput) {
-		String firstToken = rawInput.substring(0, rawInput.indexOf(' ')).toLowerCase();
-		switch (firstToken) {
+	private Command.Type getCmdType (String firstToken) {
+		switch (firstToken.toLowerCase()) {
 		
 			case "a" :		// Fallthrough
 			case "add" :	// Fallthrough
@@ -54,4 +106,7 @@ public class Parser implements ParserInterface {
 		}
 	}
 
+//	private Celebi.DataType getField (String fieldToken) {
+//		
+//	}
 }
--- src/parser/ParserInterface.java
+++ src/parser/ParserInterface.java
@@ -1,6 +1,18 @@
 package parser;
 
+import java.util.Date;
+import common.Celebi;
+
 public interface ParserInterface {
 	public void init();
 	public Command parseCommand(String s);
+	
+	// Manually create 
+
+	public Command makeAdd (String name, Date start, Date end);
+	public Command makeUpdate (int taskUID, Celebi.DataType fieldType, Object newValue);
+	public Command makeDelete (int taskUID);
+	public Command makeQuit ();
+	public Command makeInvalid ();
+	
 }
