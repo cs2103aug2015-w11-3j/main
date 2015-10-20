dfe1b1016f8f881a3daa3343cced86b101c245af
liuyang <melody9951213@gmail.com>
2015-10-21 00:55:40 +0800

    added error handling for configuration

--- src/common/Configuration.java
+++ src/common/Configuration.java
@@ -1,63 +1,66 @@
 package common;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.Scanner;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
-import storage.BadFileContentException;
-
+/*
+ * Possible errors and solutions: 
+ * 1. configuration file not found -> recreate write in default settings
+ * 2. configuration file cannot be parsed -> rewrite in default settings
+ * 3. configuration file does not have certain property -> set to default
+ */
 public class Configuration {
 
+	private final String CONFIG_DIRECTORY = "bin/config.json";
+	private final String KEY_STORAGE_LOCATION = "STORAGE_LOCATION";
+	private final String DEFAULT_VALUE_STORAGE_LOCATION = "bin/task.json";
+	
 	static Configuration instance;
-
-	private final String CONFIG_DIRECTORY = "bin/config.json"; // Config to store global
-														// settings
 	
 	private File configFile;
 	private Scanner configReader;
-	
+	private Writer configWriter;
 	private String configStorageLocation;
-
-	public static Configuration getInstance() throws BadFileContentException {
+	
+	
+	public static Configuration getInstance() {
 		if (instance == null) {
 			instance = new Configuration();
 		}
 		return instance;
 	}
 
-	private Configuration() throws BadFileContentException {
+	private Configuration() {
 		try {
 			findConfigFile();
-			
-			parseConfigFile();
-			
-		} catch (ClassCastException e) {
-			throw new BadFileContentException("Bad file format");
+			readProperties();
+		} catch (IOException e) {
+			System.out.println(e);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
 	
-	private void findConfigFile() {
-		try {
-			configFile = new File(CONFIG_DIRECTORY);
-			
-			if(!configFile.exists()) {
-				// if not found, re-create and set to default configuration
-				configFile.createNewFile();
-			}
-			
-			configReader = new Scanner(configFile);
-			configReader.useDelimiter("\\Z");
-		} catch (Exception e) {
-			System.out.println(e);
+	private void findConfigFile() throws IOException {
+		configFile = new File(CONFIG_DIRECTORY);
+		
+		if(!configFile.exists()) {
+			// if not found, re-create and set to default configuration
+			configFile.createNewFile();
+			reset();
 		}
+		
+		configReader = new Scanner(configFile);
+		configReader.useDelimiter("\\Z");
 	}
 	
-	private void parseConfigFile() throws BadFileContentException {
+	private void readProperties() throws IOException {
 		try {
 			String plainText = "";
 			if (configReader.hasNext()) {
@@ -65,25 +68,39 @@ public class Configuration {
 			}
 			
 			JSONObject parsedResult = (JSONObject) JSONValue.parse(plainText);
-			
 			if (parsedResult == null) {
 				reset();
 			}  
 			
-			configStorageLocation = (String) parsedResult.get("STORAGE_LOCATION");
-			
+			configStorageLocation = (String) parsedResult.get(KEY_STORAGE_LOCATION);
 			if(configStorageLocation == null) {
-				configStorageLocation = "";
+				configStorageLocation = DEFAULT_VALUE_STORAGE_LOCATION;
 			}
+			
+			writeBack();
 		} catch (ClassCastException e) {
-			reset();;
-		} catch (Exception e) {
-			System.out.println(e);
+			reset();
 		}
 	}
 	
-	private void reset() {
+	private void reset() throws IOException {
+		// set all properties to default value
+		configStorageLocation = DEFAULT_VALUE_STORAGE_LOCATION;
 		
+		// write to the configuartion file
+		writeBack();
+	}
+	
+	private void writeBack() throws IOException {
+		JSONObject configJson = new JSONObject();
+		configJson.put(KEY_STORAGE_LOCATION, DEFAULT_VALUE_STORAGE_LOCATION);
+		
+		configWriter = new BufferedWriter(new FileWriter(CONFIG_DIRECTORY));
+		String text = JSONValue.toJSONString(configJson);
+		
+		configWriter.write(text);
+		configWriter.close();
+		configWriter = null;
 	}
 	
 	public String getUsrFileDirectory() {
