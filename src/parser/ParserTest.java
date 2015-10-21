f25713373f4849fe57867dbcfacc46a8bba1a00a
Yijin <leowyijin@gmail.com>
2015-10-21 09:55:54 +0800

    parser v0.2 ready

---
+++ src/parser/ParserTest.java
@@ -0,0 +1,14 @@
+package parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParserTest {
+
+	@Test
+	public void testParseCommand() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
