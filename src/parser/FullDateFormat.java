8c49e8960b71217200ae8298101530da88292c65
Yijin <leowyijin@gmail.com>
2015-10-25 20:57:37 +0800

    FullDateFormat class completed

--- src/parser/FullDateFormat.java
+++ src/parser/FullDateFormat.java
@@ -9,26 +9,42 @@ import java.util.Locale;
 
 public class FullDateFormat extends SimpleDateFormat {
 	
+	
+	
 	// time before date
+	private static final String REGEX_TD_A = 
+			"hh*mma*dd*MM*yyyy";
+	private static final String REGEX_TD_B = 
+			"hh*mm*a*dd*MM*yyyy";
+	private static final String REGEX_TD_C = 
+			"HH*mm*dd*MM*yyyy";
 	private final DateFormat DF_TD_A;
 	private final DateFormat DF_TD_B;
 	private final DateFormat DF_TD_C;
+	
 	// date before time
+	private static final String REGEX_DT_A = 
+			"dd*MM*yyyy*hh*mma";
+	private static final String REGEX_DT_B = 
+			"dd*MM*yyyy*hh*mm*a";
+	private static final String REGEX_DT_C = 
+			"dd*MM*yyyy*HH*mm";
 	private final DateFormat DF_DT_A;
 	private final DateFormat DF_DT_B;
 	private final DateFormat DF_DT_C;
 	
+	// All dateformats to loop through
 	private final DateFormat[] FULL_DF_ARRAY;
 
 	FullDateFormat () {
 		
-		DF_TD_A = new SimpleDateFormat("hh*mma*dd*MM*yyyy");
-		DF_TD_B = new SimpleDateFormat("hh*mm*a*dd*MM*yyyy");
-		DF_TD_C = new SimpleDateFormat("HH*mm*dd*MM*yyyy");
+		DF_TD_A = new SimpleDateFormat(REGEX_TD_A);
+		DF_TD_B = new SimpleDateFormat(REGEX_TD_B);
+		DF_TD_C = new SimpleDateFormat(REGEX_TD_C);
 
-		DF_DT_A = new SimpleDateFormat("dd*MM*yyyy*hh*mma");
-		DF_DT_B = new SimpleDateFormat("dd*MM*yyyy*hh*mm*a");
-		DF_DT_C = new SimpleDateFormat("dd*MM*yyyy*HH*mm");
+		DF_DT_A = new SimpleDateFormat(REGEX_DT_A);
+		DF_DT_B = new SimpleDateFormat(REGEX_DT_B);
+		DF_DT_C = new SimpleDateFormat(REGEX_DT_C);
 		
 		FULL_DF_ARRAY = new DateFormat[] {
 				DF_TD_A, DF_TD_B, DF_TD_C,
