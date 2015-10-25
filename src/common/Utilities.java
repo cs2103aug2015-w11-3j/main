a5fecef1a7c7c2080d3142e073728004d03dbf98
Ken <kenlsm@u.nus.edu>
2015-10-25 22:46:57 +0800

    Refactor Actions

--- src/common/Utilities.java
+++ src/common/Utilities.java
@@ -3,17 +3,23 @@ package common;
 import java.util.Date;
 
 /**
- * Class containing miscelleous function.
- * Should not have dependency on non-java classes
+ * Class containing miscelleous function. Should not have dependency on non-java
+ * classes
  */
 public final class Utilities {
 
-	public static boolean verifyDate(Date dateStart, Date dateEnd) {
-		if (dateStart != null && dateEnd != null) {
-			if (dateStart.after(dateEnd)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public static boolean verifyDate(Date dateStart, Date dateEnd) {
+        if (dateStart != null && dateEnd != null) {
+            if (dateStart.after(dateEnd)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static String formatString(String s, Object... args){
+        String formatted = String.format(s, args);
+        return formatted;
+        
+    }
 }
