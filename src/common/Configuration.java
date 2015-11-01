209b91022e5f2c4859363895130e1fca34909b21
liuyang <melody9951213@gmail.com>
2015-11-01 13:29:26 +0800

    enable default start/end time cofiguration

--- src/common/Configuration.java
+++ src/common/Configuration.java
@@ -22,18 +22,27 @@ public class Configuration implements ConfigurationInterface {
 
     private final String CONFIG_DIRECTORY = "bin/config.json";
     private final String KEY_STORAGE_LOCATION = "STORAGE_LOCATION";
+    private final String KEY_DEFAULT_START_TIME = "DEFAULT_START_TIME";
+    private final String KEY_DEFAULT_END_TIME = "DEFAULT_END_TIME";
+    
     private final String DEFAULT_VALUE_STORAGE_LOCATION = "bin";
+    private final String DEFAULT_VALUE_DEFAULT_START_TIME = "00:00";
+    private final String DEFAULT_VALUE_DEFAULT_END_TIME = "23:59";
     
     private final String MESSAGE_INVALID_STORAGE_LOCATION = "%1$s is not a valid path";
+    private final String MESSAGE_INVALID_DEFAULT_START_TIME = "Start time %1$s is invalid";
+    private final String MESSAGE_INVALID_DEFAULT_END_TIME = "End time %1$s is invalid";
     
-    private final String MESSAGE_RESET_STORAGE_LOCATION = "Storage location set to " + CONFIG_DIRECTORY;
+    private final String MESSAGE_RESET_STORAGE_LOCATION = "Storage location set to " + DEFAULT_VALUE_STORAGE_LOCATION;
+    private final String MESSAGE_RESET_DEFAULT_START_TIME = "Storage location set to " + DEFAULT_VALUE_DEFAULT_START_TIME;
+    private final String MESSAGE_RESET_DEFAULT_END_TIME = "Storage location set to " + DEFAULT_VALUE_DEFAULT_END_TIME;
 
     static Configuration instance;
 
     private File configFile;
     private Scanner configReader;
     private Writer configWriter;
-    private String configStorageLocation;
+    private String configStorageLocation, configDefaultStartTime, configDefaultEndTime;
 
     public static ConfigurationInterface getInstance() {
         if (instance == null) {
@@ -43,16 +52,45 @@ public class Configuration implements ConfigurationInterface {
     }
 
     public String getUsrFileDirectory() {
-        Log.log("usr file is " + configStorageLocation, this.getClass());
         return configStorageLocation;
+    }
+    
+    public Time getDefaultStartTime() {
+        return new Time(configDefaultStartTime);
+    }
+    
+    public Time getDefaultEndTime() {
+        return new Time(configDefaultEndTime);
     }
 
     public void setUsrFileDirector(String newDir) throws IOException {
-        configStorageLocation = newDir;
+    	if (isValidPath(newDir)) {
+    		configDefaultStartTime = newDir;
+    		
+    		// write to the configuration file
+            writeBack();
+            Log.log("usr file moved to " + newDir, this.getClass());
+    	}
+    }
 
-        // write to the configuration file
-        writeBack();
-        Log.log("usr file moved to " + newDir, this.getClass());
+    public void setDefaultStartTime(String newTime) throws IOException {
+    	if (isValidTime(newTime)) {
+    		configDefaultStartTime = newTime;
+    		
+    		// write to the configuration file
+            writeBack();
+            Log.log("default start time reset to " + newTime, this.getClass());
+    	} 
+    }
+    
+    public void setDefaultEndTime(String newTime) throws IOException {
+    	if (isValidTime(newTime)) {
+    		configDefaultEndTime = newTime;
+    		
+    		// write to the configuration file
+            writeBack();
+            Log.log("default end time reset to " + newTime, this.getClass());
+    	}
     }
 
     private Configuration() {
@@ -93,21 +131,33 @@ public class Configuration implements ConfigurationInterface {
 
             configStorageLocation = (String) parsedResult.get(KEY_STORAGE_LOCATION);
             
-            if (configStorageLocation == null || !isValidPath(configStorageLocation)) {
-            	logInvalidLocation();
+            if (!isValidPath(configStorageLocation)) {
             	resetStorageLocation();
+            	logError(MESSAGE_INVALID_STORAGE_LOCATION, configStorageLocation, MESSAGE_RESET_STORAGE_LOCATION);
             } 
-
+            
+            configDefaultStartTime = (String) parsedResult.get(KEY_DEFAULT_START_TIME);
+            if (!isValidTime(configDefaultStartTime)) {
+            	resetDefaultStartTime();
+            	logError(MESSAGE_INVALID_DEFAULT_START_TIME, configDefaultStartTime, MESSAGE_RESET_DEFAULT_START_TIME);
+            } 
+            
+            configDefaultEndTime = (String) parsedResult.get(KEY_DEFAULT_END_TIME);
+            if (!isValidTime(configDefaultEndTime)) {
+            	resetDefaultEndTime();
+            	logError(MESSAGE_INVALID_DEFAULT_END_TIME, configDefaultEndTime, MESSAGE_RESET_DEFAULT_END_TIME);
+            } 
+            
             writeBack();
         } catch (ClassCastException e) {
         	resetAll();
         }
     }
     
-    private void logInvalidLocation() {
-    	String formatted = Utilities.formatString(MESSAGE_INVALID_STORAGE_LOCATION, configStorageLocation);
+    private void logError(String invalidMsg, String arg, String resetMsg) {
+    	String formatted = Utilities.formatString(invalidMsg, arg);
     	Log.log(formatted);
-    	Log.log(MESSAGE_RESET_STORAGE_LOCATION);
+    	Log.log(resetMsg);
     }
     
     private void resetAll() throws IOException {
@@ -122,8 +172,29 @@ public class Configuration implements ConfigurationInterface {
     	configStorageLocation = DEFAULT_VALUE_STORAGE_LOCATION;
     }
     
+    private void resetDefaultStartTime() {
+    	configDefaultStartTime = DEFAULT_VALUE_DEFAULT_START_TIME;
+    }
+    
+    private void resetDefaultEndTime() {
+    	configDefaultEndTime = DEFAULT_VALUE_DEFAULT_END_TIME;
+    }
+    
     private boolean isValidPath(String path) {
+    	if (path == null) {
+    		return false;
+    	}
+    	
     	return isValidPathName(path) && isExistingPath(path);
+    }
+    
+    private boolean isValidTime(String str) {
+    	if (str == null) {
+    		return false;
+    	}
+    	
+    	Time t = new Time(str);
+    	return t.isValid();
     }
     
     private boolean isValidPathName(String path) {
--- src/common/ConfigurationInterface.java
+++ src/common/ConfigurationInterface.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 
 public interface ConfigurationInterface {	
     public String getUsrFileDirectory();
+    public Time getDefaultStartTime();
+    public Time getDefaultEndTime();
     
     public void setUsrFileDirector(String newDir) throws IOException;
+    public void setDefaultStartTime(String newTime) throws IOException;
+    public void setDefaultEndTime(String newTime) throws IOException;
 }
