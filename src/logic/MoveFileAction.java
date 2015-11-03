7ae2711ab1c563846c255abb1ae2ad572e18b163
liuyang <melody9951213@gmail.com>
2015-11-03 23:46:00 +0800

    handle the error when input path is invalid

--- src/logic/MoveFileAction.java
+++ src/logic/MoveFileAction.java
@@ -1,16 +1,14 @@
 package logic;
 
 import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import common.Configuration;
 import common.ConfigurationInterface;
 import common.TasksBag;
 import common.Utilities;
 import logic.exceptions.LogicException;
-import logic.exceptions.PathNotExistsException;
 import parser.Command;
 import storage.StorageInterface;
 
@@ -19,6 +17,7 @@ public class MoveFileAction implements Action {
     private static final String USR_MSG_MOVE_ERROR = "Failed to move storage file";
     private static final String USR_MSG_MOVE_OK = "Storage file moved!";
     private static final String USR_MSG_MOVE_ERROR_FILE_EXISTING = "Storage file in %1s already exists";
+    private static final String USR_MSG_MOVE_ERROR_INVALID_PATH = "The directory %1s does not exists";
     private static final String USR_MSG_MOVE_INVALID_PATH = "Invalid Path!";
     
     private Command cCommand;
@@ -48,11 +47,13 @@ public class MoveFileAction implements Action {
 
             return fb;
         } catch (FileAlreadyExistsException e) {
-        	System.out.println("sb2");
         	String fommatted = Utilities.formatString(USR_MSG_MOVE_ERROR_FILE_EXISTING, cPath.toString());
         	throw new LogicException(fommatted);
+        } catch (NoSuchFileException e) {
+        	String fommatted = Utilities.formatString(USR_MSG_MOVE_ERROR_INVALID_PATH, cPath.toString());
+        	throw new LogicException(fommatted);
         } catch (Exception e) {
-//        	System.out.println(e);
+        	System.out.println(e);
         	System.out.println("sb2");
         	return new Feedback(cCommand, cBag, USR_MSG_MOVE_OK);
 
